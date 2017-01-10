0664e45a5a0c08149da2ae7f83bf0443f7690120
Deepak Vig <deepakvig@Deepaks-MacBook.local>
2017-01-10 17:30:08 +0530

    TodoApp - Search filter fixed

--- public/bundle.js
+++ public/bundle.js
@@ -25520,14 +25520,18 @@
 			this.setState({ todos: updatedTodos });
 		},
 		render: function render() {
-			var todos = this.state.todos;
+			var _state = this.state,
+			    todos = _state.todos,
+			    showCompleted = _state.showCompleted,
+			    searchText = _state.searchText;
 
+			var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
 			return React.createElement(
 				'div',
 				null,
 				React.createElement(TodoSearch, { onSearch: this.handleSearch }),
-				React.createElement(TodoList, { todos: todos, onToggle: this.handleToggle }),
+				React.createElement(TodoList, { todos: filteredTodos, onToggle: this.handleToggle }),
 				React.createElement(AddTodo, { onAddTodo: this.handleAddTodo })
 			);
 		}
@@ -34067,6 +34071,34 @@
 			}
 
 			return $.isArray(todos) ? todos : [];
+		},
+		filterTodos: function filterTodos(todos, showCompleted, searchText) {
+			var filteredTodos = todos;
+
+			// showCompleted
+			filteredTodos = filteredTodos.filter(function (todo) {
+				return !todo.completed || showCompleted;
+			});
+
+			// searchText
+			filteredTodos = filteredTodos.filter(function (todo) {
+				var todoText = todo.text.toLowerCase();
+
+				return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+			});
+
+			// Sort
+			filteredTodos.sort(function (a, b) {
+				if (!a.completed && b.completed) {
+					return -1;
+				} else if (a.completed && !b.completed) {
+					return 1;
+				} else {
+					return 0;
+				}
+			});
+
+			return filteredTodos;
 		}
 	};
 
