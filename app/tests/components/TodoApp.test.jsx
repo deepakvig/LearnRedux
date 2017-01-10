c8d5a5913ef797d3239a1a3009e705406514c6fe
Deepak Vig <deepakvig@Deepaks-MacBook.local>
2017-01-10 15:39:43 +0530

    Add Todo Fixed - Tested

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,8 +1,21 @@
+var React = require('react');
 var expect = require('expect');
 var TodoApp = require('TodoApp');
+var $ = require('jQuery');
+var TestUtils = require('react/lib/ReactTestUtils');
 
 describe('TodoApp', () => {
 	it('should exist', () => {
 		expect(TodoApp).toExist();
 	});	
+	
+	it('should add todo to the todos state on handleAddTodo', () => {
+		var todoText = 'test text';
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+		
+		todoApp.setState({todos: []});
+		todoApp.handleAddTodo(todoText);
+		
+		expect(todoApp.state.todos[0].text).toBe(todoText);
+	});
 })
